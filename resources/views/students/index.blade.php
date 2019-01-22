3249414a07297dd4d7cd1fb18c08307a83ccbe6a
Bruno Firme <bruno.firme@digitalbrasilia.com.br>
2019-01-22 21:02:29 -0200

    Views Students

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -20,23 +20,18 @@
     <div class="container">
         <div class="card">
             <div class="card-header">
-                Contatos
-                <a href="{{ url('contatos/add') }}" class="btn btn-primary btn-sm float-right">Novo</a>
+                Estudantes
+                <a href="{{ url('student/add') }}" class="btn btn-primary btn-sm float-right">Novo</a>
             </div>
             <div class="card-body p-0">
                 <div class="table-responsive border-0">
                     <table class="table table-hover" style="margin-bottom: inherit">
                         <tbody>
-                        @foreach ($contatos as $contato)
+                        @foreach ($students as $student)
                             <tr>
-                                <td>
-                                    <a href="{{ url('contatos/'.$contato->id) }}">
-                                        <img src="{{ $contato->avatar_image }}" class="img-avatar-xs">
-                                    </a>
-                                </td>
-                                <td><a class='a-line' href="{{ url('contatos/'.$contato->id) }}">{{ $contato->nome }}</a></td>
-                                <td class="d-none d-md-table-cell"><a class='a-line' href="{{ url('contatos/'.$contato->id) }}">{{ $contato->telefone }}</a></td>
-                                <td class="d-none d-md-table-cell"><a class='a-line' href="{{ url('contatos/'.$contato->id) }}">{{ $contato->email }}</a></td>
+                                <td><a class='a-line' href="{{ url('students/'.$student->id) }}">{{ $student->name }}</a></td>
+                                <td class="d-none d-md-table-cell"><a class='a-line' href="{{ url('students/'.$student->id) }}">{{ $student->born_at }}</a></td>
+                                <td class="d-none d-md-table-cell"><a class='a-line' href="{{ url('students/'.$student->id) }}">{{ $student->team_id }}</a></td>
                             </tr>
                         @endforeach
                         </tbody>
