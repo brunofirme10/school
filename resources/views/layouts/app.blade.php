5d307ccc07c8e7fc66274329664d4cff53e1051b
Bruno Firme <bruno.firme@digitalbrasilia.com.br>
2019-01-22 21:25:06 -0200

    Login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'School') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -28,7 +28,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Minha Agenda') }}
+                    {{ config('app.name', 'School') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -43,7 +43,7 @@
                     @else
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('contatos') }}">Contatos <span class="sr-only">(current)
+                        <a class="nav-link" href="{{ url('home') }}">Alunos <span class="sr-only">(current)
                     </li>
 
                     @endguest
