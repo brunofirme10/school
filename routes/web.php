8f5b8c4cb974aae34c492f9ec175577b93510976
Bruno Firme <bruno.firme@digitalbrasilia.com.br>
2019-01-22 21:07:45 -0200

    Routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,15 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get("student", "StudentController@index");
+Route::get("students", "StudentController@index")->name('home');
+
+//rota para a classe Students com autenticação
+Route::group(['middleware' => 'auth', 'prefix' => 'students'], function (){
+    Route::get('/', "StudentController@index");
+    Route::get('/add', 'StudentController@create');
+    Route::post('/', 'StudentController@store');
+    Route::get('{id}', 'StudentController@show');
+    Route::get('/edit/{id}', 'StudentController@edit');
+    Route::put('{id}', 'StudentController@update');
+    Route::delete('{id}', 'StudentController@destroy');
+});
